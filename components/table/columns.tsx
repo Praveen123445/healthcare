852814e2a4f3da9d3b2f36d7d33c1ff54acd27d4
Praveen123445 <Pravin324@gmail.com>
2024-09-14 15:39:28 +0530

    first commit

---
+++ components/table/columns.tsx
@@ -0,0 +1,101 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "../ui/button";
+import StatusBadge from "../StatusBadge";
+import { formatDateTime } from "@/lib/utils";
+import Image from "next/image";
+import { Doctors } from "@/constants";
+import AppointmentModal from "../AppointmentModal";
+
+// Define types
+export type Patient = {
+  id: string;
+  name: string;
+};
+
+export type Payment = {
+  id: string;
+  amount: number;
+  status: "pending" | "processing" | "success" | "failed";
+  email: string;
+  patient?: Patient; // Optional
+};
+
+export const columns: ColumnDef<Payment>[] = [
+  {
+    header: 'ID',
+    cell: ({ row }) => <p className="text-14-medium">{row.index + 1}</p>,
+  },
+  {
+    accessorKey: 'patient',
+    header: 'Patient',
+    cell: ({ row }) => {
+      const patient = row.original.patient;
+      return (
+        <p className="text-14-medium">
+          {patient ? patient.name : "N/A"} {/* Display 'N/A' if patient info is missing */}
+        </p>
+      );
+    },
+  },
+  {
+    accessorKey: 'status',
+    header: 'Status',
+    cell: ({ row }) => (
+        <div className="min-w-[115px]">
+            <StatusBadge status={row.original.status}/>
+        </div>
+    )
+  },
+  {
+    accessorKey: 'schedule',
+    header: 'Appointment',
+    cell: ({ row }) => (
+        <p className="text-14-regular min-w-[100px]">
+            {formatDateTime(row.original.schedule).dateTime}
+        </p>
+    )
+  },
+  {
+    accessorKey: 'primaryPhysician',
+    header: () => 'Doctor',
+    cell: ({ row }) => {
+      const doctor = Doctors.find((doc) => doc.name === row.original.primaryPhysician)
+
+      return (
+        <div className="flex items-center gap-3">
+            <Image 
+              src={doctor?.image}
+              alt={doctor.name}
+              height={100}
+              width={100}
+              className="size-8"
+            />
+            <p className="whitespace-nowrap">
+                Dr. {doctor?.name}
+            </p>
+        </div>
+      )
+    },
+  },
+  {
+    id: 'actions',
+    header: () => <div className="pl-4">Actions</div>,
+    cell: ({ row }) => {
+        return (
+            <div className="flex gap-1">
+                <AppointmentModal/>
+            </div>
+        )
+    },
+  },
+];
